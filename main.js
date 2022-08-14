29ca0fb4b97cb7d3f28c19e5e9c354439290ff79
chris-ac-malone <chris.ac.malone@gmail.com>
2022-08-14 03:16:16 -0600

    Finished functionality.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, ipcMain} = require('electron')
 const path = require('path')
 
 function createWindow () {
@@ -41,3 +41,4 @@ app.on('window-all-closed', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+ipcMain.on("values-changed", (event, data) => console.log(data))
--- preload.js
+++ preload.js
@@ -10,3 +10,9 @@ window.addEventListener('DOMContentLoaded', () => {
     replaceText(`${type}-version`, process.versions[type])
   }
 })
+
+const { contextBridge, ipcRenderer } = require("electron")
+
+contextBridge.exposeInMainWorld("electron", {
+    send: (channel, payload) => ipcRenderer.send(channel, payload),
+})
